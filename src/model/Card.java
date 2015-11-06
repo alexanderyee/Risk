6603df466a7b51f901e3f680c78255139a9853f1
Reaper Romero <reaperr@email.arizona.edu>
2015-11-06 14:44:02 -0700

    Risk

--- src/model/Card.java
+++ src/model/Card.java
@@ -4,10 +4,11 @@ public class Card {
 
 	//MEMBER VARIABLES
 	private CardType type;
-	
+	private Countries country;
 	//CONSTRUCTOR
-	public Card(CardType t) {
+	public Card(CardType t,Countries c) {
 		type = t;
+	    country=c;
 	}
 	
 }
