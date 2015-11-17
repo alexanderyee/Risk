635d8a832f2f65bd09cc09205ce5b0c31b59e68a
Alexander Yee <alexanderyee@email.arizona.edu>
2015-11-17 10:15:10 -0700

    Added user input to GUI: needs to be able to send/receive messages from Game.java

--- src/view/GraphicalView.java
+++ src/view/GraphicalView.java
@@ -5,9 +5,10 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,6 +22,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import model.Game;
 
@@ -34,10 +36,11 @@ public class GraphicalView extends JFrame implements Observer
     }
 
     private Game game;
-    private BufferedImage map;
+    private Image map;
     private MapPanel mapPanel;
     private JButton addArmy;
     private JTextArea gameInfo;
+    private JTextField console;
     private int X, Y;
 
     public GraphicalView()
@@ -45,13 +48,16 @@ public class GraphicalView extends JFrame implements Observer
         try
         {
             map = ImageIO.read(new File("./images/map.jpg"));
+
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
-        int imgWidth = map.getWidth();
-        int imgHeight = map.getHeight();
+        int imgWidth = 1600 * 3 / 4;
+        int imgHeight = 1062 * 3 / 4;
+        map = map.getScaledInstance(imgWidth, imgHeight,
+                java.awt.Image.SCALE_SMOOTH);
         this.setTitle("Risk - Team Rocket Industries");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -62,8 +68,16 @@ public class GraphicalView extends JFrame implements Observer
         mapPanel.setLayout(null);
         mapPanel.setSize(new Dimension(imgWidth, imgHeight));
         mapPanel.setLocation(0, 0);
-       
+        gameInfo = new JTextArea();
+        gameInfo.setSize(new Dimension(screensize.width - imgWidth, imgHeight));
+        gameInfo.setLocation(imgWidth, 0);
+        console = new JTextField();
+        console.setSize(new Dimension(screensize.width - imgWidth, 30));
+        console.setLocation(imgWidth, imgHeight);
+        console.addActionListener(new ConsoleListener());
         this.add(mapPanel);
+        this.add(gameInfo);
+        this.add(console);
         repaint();
     }
 
@@ -149,22 +163,43 @@ public class GraphicalView extends JFrame implements Observer
     public void update(Observable o, Object arg)
     {
         repaint();
-        gameInfo.setText(o.toString());
+        gameInfo.setText((String) arg); // make sure to send a string whenever
+                                        // you update to update the game
+                                        // information text area.
     }
 
     private class MapPanel extends JPanel
     {
-        public MapPanel(){
-          
+        public MapPanel()
+        {
+
         }
+
         @Override
         public void paintComponent(Graphics g)
         {
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
+
             g2.drawImage(map, 0, 0, null); // will consider lower resolutions
                                            // and scroll panes
 
         }
+    }
+
+    private class ConsoleListener implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {// TODO: Implement this method
+            /*
+             * This happens whenever enter button is pressed in the JTextField
+             * for the console. I'm not sure what you guys want here since it
+             * has to send messages to Game.java
+             */
+
+        }
+
     }
 }
